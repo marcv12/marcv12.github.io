844ff361d3ce3537946a68f7df8befb0741912f9
marcv12 <marcviolides@yahoo.com>
2024-03-05 23:30:07 +0000

    quicker erasing

--- assets/js/greetingsAnimation.js
+++ assets/js/greetingsAnimation.js
@@ -34,7 +34,7 @@ function updateGreeting() {
         }
     }
     element.innerHTML = currentText;
-    setTimeout(updateGreeting, currentStep === 'typing' ? 100 : 60); // Quicker typing, quicker erasing
+    setTimeout(updateGreeting, currentStep === 'typing' ? 80 : 40); // Quicker typing, quicker erasing
 }
 
 // Start the cycle on page load
